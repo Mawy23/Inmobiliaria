a2d55af1f743799758902b05931210ed1750967d
belencstr <belencasta03@gmail.com>
2024-12-17 11:58:44 +0100

    mas arreglos

--- App/Views/usuarios/profile/profile.php
+++ App/Views/usuarios/profile/profile.php
@@ -19,12 +19,6 @@
             <li class="nav-item">
                 <a class="nav-link <?= $active_tab === 'favoritos' ? 'active' : '' ?>" id="favoritos-tab" data-toggle="tab" href="#favoritos" role="tab" aria-controls="favoritos" aria-selected="<?= $active_tab === 'favoritos' ? 'true' : 'false' ?>">Favoritos</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link <?= $active_tab === 'historial' ? 'active' : '' ?>" id="historial-tab" data-toggle="tab" href="#historial" role="tab" aria-controls="historial" aria-selected="<?= $active_tab === 'historial' ? 'true' : 'false' ?>">Historial de Citas</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link <?= $active_tab === 'mis_propiedades' ? 'active' : '' ?>" id="mis_propiedades-tab" data-toggle="tab" href="#mis_propiedades" role="tab" aria-controls="mis_propiedades" aria-selected="<?= $active_tab === 'mis_propiedades' ? 'true' : 'false' ?>">Mis Propiedades</a>
-            </li>
         <?php endif; ?>
         <li class="nav-item">
             <a class="nav-link <?= $active_tab === 'citas' ? 'active' : '' ?>" id="citas-tab" data-toggle="tab" href="#citas" role="tab" aria-controls="citas" aria-selected="<?= $active_tab === 'citas' ? 'true' : 'false' ?>">Citas</a>
@@ -49,12 +43,6 @@
         <?php if ($rol === 'cliente'): ?>
             <div class="tab-pane fade <?= $active_tab === 'favoritos' ? 'show active' : '' ?>" id="favoritos" role="tabpanel" aria-labelledby="favoritos-tab">
                 <?php include 'favoritos.php'; ?>
-            </div>
-            <div class="tab-pane fade <?= $active_tab === 'historial' ? 'show active' : '' ?>" id="historial" role="tabpanel" aria-labelledby="historial-tab">
-                <?php include 'historial.php'; ?>
-            </div>
-            <div class="tab-pane fade <?= $active_tab === 'mis_propiedades' ? 'show active' : '' ?>" id="mis_propiedades" role="tabpanel" aria-labelledby="mis_propiedades-tab">
-                <?php include 'mis_propiedades.php'; ?>
             </div>
         <?php endif; ?>
         <div class="tab-pane fade <?= $active_tab === 'citas' ? 'show active' : '' ?>" id="citas" role="tabpanel" aria-labelledby="citas-tab">
